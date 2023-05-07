e13e1d309b5fa54ca456be94e70284d4adbfd280
jonathanSLimaRio <jonathan.planeje@gmail.com>
2023-05-07 13:46:32 -0300

    feat: initial project

---
+++ src/views/WeatherView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="weather">
+    <h1>This is an weather page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .weather {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
